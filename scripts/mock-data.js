09a4843af594ab06e091142989e17ca1d991560e
bartolomej <bartolomej.kozorog@gmail.com>
2019-11-03 15:19:01 +0100

    Implemented test data fetching via randomuser.me API.

--- scripts/mock-data.js
+++ scripts/mock-data.js
@@ -1,5 +1,6 @@
 const getRepository = require('typeorm').getRepository;
 const uuid = require('uuid/v4');
+const fetch = require('node-fetch');
 
 const Category = require('../src/modules/education/Category');
 const Subcategory = require('../src/modules/education/Subcategory');
@@ -11,36 +12,10 @@ const userDb = require('../src/modules/user/db/repository');
 const educationDb = require('../src/modules/education/db/repository');
 const emailDb = require('../src/modules/email/db/repository');
 
-const users = [
-  'jakic12',
-  'bartKoz',
-  'kiko',
-  'zak',
-  'jakic122',
-  'bartK3oz',
-  'kikso',
-  'zdak',
-  'jakfdic12',
-  'barqtKoz',
-  'kifko',
-  'zcak',
-  'jakdic12',
-  'bavrtKoz',
-  'kisko',
-  'zxack',
-  'jakixc12',
-  'bavrxtKoz',
-  'kikxo',
-  'zaxk',
-  'jakxic12',
-  'barztKoz',
-  'kifgdko',
-  'zafk',
-  'jakicsds12',
-  'baartKoz',
-  'kiqko',
-  'zgak',
-];
+
+/** PARAMETERS **/
+const numberOfTestUsers = 30;
+const numberOfTestEmails = 20;
 
 (async function () {
   await require('../src/setup/enviroment')();
@@ -61,8 +36,8 @@ const users = [
 
   try {
     await insertEducationData();
-    await insertUserData(users);
-    await insertMailData(users, 6);
+    await insertUserData(numberOfTestUsers);
+    await insertMailData(numberOfTestEmails);
     console.log('Test data successfully inserted in db.');
     process.exit();
   } catch (e) {
@@ -73,70 +48,107 @@ const users = [
 })();
 
 async function insertEducationData () {
-  let matematika = await educationDb.saveCategory(new Category(
+  let matematika = await createCategory(
     'Matematika',
     'Vse o matematiki...',
-  ));
-  let racunalnistvo = await educationDb.saveCategory(new Category(
+  );
+  let racunalnistvo = await createCategory(
     'Racunalnistvo',
     'Vse o racunalnistvu...',
-  ));
-  let stevila = await educationDb.saveSubcategory(new Subcategory(
+  );
+  let stevila = await createSubcategory(
     'Stevila',
     'V tem poglavju se bomo ukvarjali z stevili',
     matematika
-  ));
-  let programiranje = await educationDb.saveSubcategory(new Subcategory(
+  );
+  let programiranje = await createSubcategory(
     'Programiranje',
     'SubCategory2 description',
     racunalnistvo
-  ));
-  await educationDb.saveCourse(new Course(
+  );
+  await createCourse(
     'Vizualizacija kompleksnih stevil',
     `Kaj so kompleksna in realna stevila ? Izdelajte preprosto vizualizacijo...`,
     ['stevila', 'programiranje', 'vizualizacije', 'javascript'],
     '# Test content',
     [programiranje, stevila]
-  ));
-  await educationDb.saveCourse(new Course(
+  );
+  await createCourse(
     'Osnove programiranja v Pythony',
     `Tukaj se boste naucili osnove programskega jezika Python, 
      ki je zelo popularen med mladimi programerji`,
     ['python', 'programiranje'],
     '# Test content',
     [programiranje]
-  ));
+  );
+
+  async function createCategory (name, description) {
+    let category = new Category(name, description);
+    category.visible = true;
+    await educationDb.saveCategory(category);
+  }
+  async function createSubcategory (name, description, category) {
+    let subcategory = new Subcategory(name, description, category);
+    subcategory.visible = true;
+    await educationDb.saveSubcategory(subcategory);
+  }
+  async function createCourse (title, description, tags, content, subcategories) {
+    let course = new Course(
+      title,
+      description,
+      tags,
+      content,
+      subcategories
+    );
+    course.visible = true;
+    await educationDb.saveCourse(course);
+  }
 }
 
-async function insertUserData (users) {
-  for (const username of users) {
-    let user = createUser(username);
+async function insertUserData (n) {
+  for (let i = 0; i < n; i++) {
+    let response = await fetch('https://randomuser.me/api/');
+    let result = await response.json();
+    let randomUser = result.results[0];
+    await createUser(
+      randomUser.login.username,
+      randomUser.registered.date,
+      randomUser.dob.date,
+      randomUser.email,
+      randomUser.picture.medium,
+      randomUser.gender
+    );
+  }
+
+  async function createUser (username, created, birth, email, avatar, gender) {
+    let user = new User();
+    user.username = username;
+    user.createdDate = created;
+    user.birthDate = birth;
+    user.email = email;
+    user.website = username + '-example.com';
+    user.interests = 'programming,design,math';
+    user.avatar = avatar;
+    user.gender = gender;
+    user.type = Math.random()*2 > 1 ? 'ADMIN' : 'USER';
     await userDb.save(user);
   }
 }
 
-function createUser (username) {
-  let user = new User();
-  user.username = username;
-  user.createdDate = new Date();
-  user.birthDate = new Date();
-  user.email = username + '@mail.com';
-  user.website = username + '-example.com';
-  user.interests = 'programming,design,math';
-  user.avatar = '/image/profile.png';
-  user.type = Math.random()*2 > 1 ? 'ADMIN' : 'USER';
-  return user;
-}
-
-async function insertMailData (users, n) {
+async function insertMailData (n) {
+  let users = await userDb.getAll();
   for (let i = 0; i < n; i++) {
-    let from = Math.round(Math.random()*n);
-    let to = Math.round(Math.random()*n);
+    let from = users[Math.round(Math.random()*n)].username;
+    let to = users[Math.round(Math.random()*n)].username;
+    await createEmail(from, to, i);
+  }
+
+  async function createEmail (fromUser, toUser, index) {
     let email = new Email();
     email.uid = uuid();
-    email.fromAddress = `${users[from]}@mail.com`;
-    email.toAddress = `${users[to]}@mail.com`;
-    email.subject = `Test mail ${i}`;
+    email.fromAddress = `${fromUser}@mail.com`;
+    email.toAddress = `${toUser}@mail.com`;
+    email.subject = `Test mail ${index}`;
     email.text = 'This is a test email. ';
     await emailDb.save(email);
   }
